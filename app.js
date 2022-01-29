b212332658a0c62aabe707e72e9e51fe4cad00f6
Baran Cakirbey <barancakirbey13@gmail.com>
2022-01-29 12:03:30 +0300

    simple landing page 1

---
+++ app.js
@@ -0,0 +1,5 @@
+const homeBtn = document.querySelector("#nav-home")
+const storeBtn = document.querySelector("#store-home")
+const ınfoBtn = document.querySelector("#ınfo-home")
+
+const homePage = document.querySelector("#panel-main")
